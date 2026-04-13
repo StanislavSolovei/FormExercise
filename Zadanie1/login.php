f9df8c1de671cda695b07b5034e4042a93449f79
StanislavSolovei <edukacjateb176@gmail.com>
2026-04-13 17:57:08 +0200

    Update login.php

--- Zadanie1/login.php
+++ Zadanie1/login.php
@@ -4,8 +4,8 @@ $login = $_POST['login'];
 $password = $_POST['password'];
 $remember = isset($_POST['remember']) ? "Zaznaczono" : "Nie zaznaczono";
 
-echo "Login: " .($login) . "<br>";
-echo "Hasło: " .($password) . "<br>";
+echo "Login: " .$login . "<br>";
+echo "Hasło: " .$password . "<br>";
 echo "Checkbox: " .$remember;
 
 ?>
