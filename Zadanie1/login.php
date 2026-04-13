f334b542f26969d85c36bb847ddd89a1fbdf258b
StanislavSolovei <edukacjateb176@gmail.com>
2026-04-13 17:33:26 +0200

    Add files via upload

---
+++ Zadanie1/login.php
@@ -0,0 +1,11 @@
+<?php
+
+$login = $_POST['login'];
+$password = $_POST['password'];
+$remember = isset($_POST['remember']) ? "Zaznaczono" : "Nie zaznaczono";
+
+echo "Login: " .($login) . "<br>";
+echo "Hasło: " .($password) . "<br>";
+echo "Checkbox: " .$remember;
+
+?>
